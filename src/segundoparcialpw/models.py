07e231741f53820c1c34a44be713e8bbed70d901
JuanDavid601 <lhdj201987@gmail.com>
2025-10-21 13:21:34 -0500

    Co-authored-by: Vale <ValeJuanda@users.noreply.github.com>

--- src/segundoparcialpw/models.py
+++ src/segundoparcialpw/models.py
@@ -1,69 +1,151 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float, Boolean, ForeignKey, Table, Text
-from sqlalchemy.ext.declarative import declarative_base
+from slugify import slugify
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import sessionmaker, relationship
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from flask import url_for
+from runpy import db
 
 
-Base = declarative_base()
+
 
 receta_ingrediente = Table(
-    'receta_ingrediente', Base.metadata,
-    Column('receta_id', Integer, ForeignKey('recetas.id'), primary_key=True),
-    Column('ingrediente_id', Integer, ForeignKey('ingredientes.id'), primary_key=True),
-    Column('cantidad', String, nullable=True)
+    'receta_ingrediente',
+    db.metadata,
+    Column('receta_id', Integer, ForeignKey('recetas.id', ondelete='CASCADE'), primary_key=True),
+    Column('ingrediente_id', Integer, ForeignKey('ingredientes.id', ondelete='CASCADE'), primary_key=True)
 )
 
-class Usuario(Base):
-    __tablename__ = 'usuarios'
-    id = Column(Integer, primary_key=True)
-    nombre = Column(String(120), nullable=False)
-    correo = Column(String(120), nullable=False, unique=True)
-    password_hash = Column(String(128), nullable=False)
-    is_admin = Column(Boolean, default=False, nullable=False)
+class User(db.Model, UserMixin):
+    
+    __tablename__ = 'Users'
 
-    recetas = relationship('Receta', back_populates='autor')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(256), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    recetas = relationship('Receta', back_populates='usuario', cascade='all, delete')
+
+
+    def __repr__(self):
+        return f'<User {self.email}>'
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password, password)
 
-class Categoria(Base):
-    __tablename__ = 'categorias'
-    id = Column(Integer, primary_key=True)
-    nombre = Column(String(80), nullable=False)
-    tipo = Column(String(80), nullable=False)
-    descripcion = Column(Text, nullable=False)
+    def save(self):
+        if not self.id:
+            db.session.add(self)
+        db.session.commit()
 
-    recetas = relationship('Receta', back_populates='categoria')
+    @staticmethod
+    def get_by_id(id):
+        return User.query.get(id)
 
-class Receta(Base):
+    @staticmethod
+    def get_by_email(email):
+        return User.query.filter_by(email=email).first()
+
+class Receta(db.Model, UserMixin):
+
     __tablename__ = 'recetas'
-    id = Column(Integer, primary_key=True)
-    nombre = Column(String(120), nullable=False)
-    categoria_id = Column(Integer, ForeignKey('categorias.id'), nullable=False)
-    autor_id = Column(Integer, ForeignKey('usuarios.id'), nullable=True)
-    tiempo = Column(Integer, nullable=False)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.column(db.string(80), ForeignKey('User.id', ondelete='CASCADE'), nullable=False)
+    name = db.column(db.String(100), nullable=False)
+    categoria = db.column(db.string(80), ForeignKey('Categoria.id'))
+    Ingredientes = db.column(db.string(250), ForeignKey('Ingredientes.id'))
+    preparacion = db.column(db.text(250), ForeignKey('Preparacion.id'), nullable=False)
+    slug = db.column(db.string(150), unique=True, nullable=False)
+    descripcion = db.column(db.text(255), nullable=False)
 
+
+    usuario = relationship('Usuario', back_populates='recetas')
     categoria = relationship('Categoria', back_populates='recetas')
-    autor = relationship('Usuario', back_populates='recetas')
+    preparacion = relationship('Preparacion', back_populates='recetas')
     ingredientes = relationship('Ingrediente', secondary=receta_ingrediente, back_populates='recetas')
-    pasos = relationship('Preparacion', back_populates='receta', cascade='all, delete-orphan', order_by='Preparacion.orden')
 
-class Ingrediente(Base):
-    __tablename__ = 'ingredientes'
-    id = Column(Integer, primary_key=True)
-    nombre = Column(String(120), nullable=False, unique=True)
+
+
+    def save(self):
+        if not self.id:
+            db.session.add(self)
+        if not self.slug:
+            self.slug = slugify(self.name)
+        saved = False
+        count = 0
+        while not saved:
+            try:
+                db.session.commit()
+                saved = True
+            except IntegrityError:
+                count += 1
+                self.slug = f'{slugify(self.name)}-{count}'
+
+    def public_url(self):
+        return url_for('show_recetas', slug=self.slug)
+    
+    @staticmethod
+    def get_by_slug(slug):
+        return Receta.query.filter_by(slug=slug).first()
+    
+    @staticmethod
+    def get_receta_by_id(id):
+        return Receta.query.get(id)
+    
+    @staticmethod
+    def get_all():
+        return Receta.query.all()
+
+class Categoria(db.Model):
+    __tablename__ = 'categorias'
+
+    id = db.column(db.Integer, primary_key=True)
+    nombre = db.column(db.String(100), nullable=False)
+    tipo= db.column(db.String(100), nullable=False)
+    descripcion = db.column(db.String(255), nullable=False)
+
+    recetas = relationship('Receta', back_populates='categoria', cascade='all, delete')
+
+    
+    def save(self):
+        if not self.id:
+            db.session.add(self)
+        db.session.commit()
+
+class Ingredientes(db.Model):
+    __tablename__ = 'Ingredientes'
+    
+    id = db.column(db.Integer, primary_key=True)
+    nombre = db.column(db.String(100), nullable=False)
+    cantidad = db.column(db.Integer, nullable=False)
+
 
     recetas = relationship('Receta', secondary=receta_ingrediente, back_populates='ingredientes')
 
-class Preparacion(Base):
-    __tablename__ = 'preparaciones'
-    id = Column(Integer, primary_key=True)
-    receta_id = Column(Integer, ForeignKey('recetas.id'), nullable=False)
-    orden = Column(Integer, nullable=False)
-    descripcion = Column(Text, nullable=False)
-    tiempo = Column(Integer, nullable=True)
+    def save(self):
+        if not self.id:
+            db.session.add(self)
+        db.session.commit()
 
-    receta = relationship('Receta', back_populates='pasos')
+class Preparacion(db.Model):
+    __tablename__ = 'Preparaciones'
+
+    id = db.column(db.Integer, primary_key=True)
+    tiempo = db.column(db.Integer, nullable=False)
+    ingredientes = db.column(db.String(100), primary_key=True)
+    descripcion = db.column(db.String(100), primary_key=True)
+
+
+    recetas = relationship('Receta', back_populates='preparacion')
+
+    def save(self):
+        if not self.id:
+            db.session.add(self)
+        db.session.commit()
